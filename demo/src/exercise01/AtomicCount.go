75e4c92b79fb58e13de0f421dc04a7e33b34cd13
xingtingbiao <xingtingbiao@znlhzl.com>
2019-01-23 14:06:42 +0800

    feature: go的练习

--- demo/src/exercise01/AtomicCount.go
+++ demo/src/exercise01/AtomicCount.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
-/**
-原子计数
-*/
 func main() {
-	atomicCount()
+	//atomicCount()
+	testLock()
 }
 
+// 原子计数
 func atomicCount() {
 	var ops uint64 = 0
 
@@ -26,4 +27,48 @@ func atomicCount() {
 	}
 	time.Sleep(time.Second)
 	fmt.Println("ops:", atomic.LoadUint64(&ops))
+}
+
+// 互斥体实例, 类似于java中的ReentrantLock类
+func testLock() {
+	state := make(map[int]int)
+	mutex := &sync.Mutex{}
+
+	var readOps uint64 = 0
+	var writeOps uint64 = 0
+	for i := 0; i < 100; i++ {
+		go func() {
+			total := 0
+			for true {
+				key := rand.Intn(5)
+				mutex.Lock()
+				total += state[key]
+				mutex.Unlock()
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for true {
+				key := rand.Intn(5)
+				val := rand.Intn(100)
+				mutex.Lock()
+				state[key] = val
+				mutex.Unlock()
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("readOps:", atomic.LoadUint64(&readOps))
+	fmt.Println("writeOps:", atomic.LoadUint64(&writeOps))
+
+	mutex.Lock()
+	fmt.Println("state:", state)
+	mutex.Unlock()
 }
