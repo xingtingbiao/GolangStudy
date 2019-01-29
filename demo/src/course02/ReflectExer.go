a25e1a5db23036c606c8368a50ef58ec005e1f1b
xingtingbiao <xingtingbiao@znlhzl.com>
2019-01-29 17:35:38 +0800

    feature: go中reflect的练习

--- demo/src/course02/ReflectExer.go
+++ demo/src/course02/ReflectExer.go
@@ -25,9 +25,19 @@ const Zero Enum = 0
 
 func testReflect02() {
 	type cat struct {
+		Name string
 	}
 	typeOfCat := reflect.TypeOf(cat{})
 	fmt.Println(typeOfCat.Name(), typeOfCat.Kind())
+
+	// 修改结构体的属性值需要用指针
+	valueOfCat := reflect.ValueOf(&cat{})
+	fmt.Println(typeOfCat.NumField())
+	fmt.Println(typeOfCat.Field(0).Name)
+	name := valueOfCat.Elem().FieldByName("Name")
+	// 赋值需要结构体的属性可导出, 即属性首字母为大写, 类似java的public 和 private
+	name.SetString("豆芽")
+	fmt.Println(name.String())
 
 	typeOfZero := reflect.TypeOf(Zero)
 	fmt.Println(typeOfZero.Name(), typeOfZero.Kind())
